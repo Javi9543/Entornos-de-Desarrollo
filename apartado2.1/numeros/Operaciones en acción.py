432d9a45a666a0c07df4711ae6331b2a354b41a3
Javier Muñoz Parra <jmunpar1307@g.educaand.es>
2025-10-25 19:46:13 +0200

    Programa finalizado

--- apartado2.1/numeros/Operaciones en acción.py
+++ apartado2.1/numeros/Operaciones en acción.py
@@ -2,3 +2,18 @@ n1 = int(input('Introduzca un numero'))
 n2 = int(input('Introduzca el segundo numero'))
 
 suma = n1 + n2
+resta = n1 - n2
+mult = n1 * n2
+div = n1 / n2
+div_ent = n1 // n2
+resto_num = n1 % n2
+pot = n1 ** n2
+
+print(suma)
+print(resta)
+print(mult)
+print(div)
+print(div_ent)
+print(resto_num)
+print(resto_num)
+print(pot)
