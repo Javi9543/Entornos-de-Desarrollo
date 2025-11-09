30b6104a3f3eff54b8f651d688991d0cb9c84f26
Javier Muñoz Parra <jmunpar1307@g.educaand.es>
2025-11-09 12:02:51 +0100

    Introducción de try con variables, palabra y longituf dentro del try

--- apartado2.1/cadenas/Longitud de una palabra.py
+++ apartado2.1/cadenas/Longitud de una palabra.py
@@ -1,4 +1,9 @@
-palabra = input('Introduzca una palabra')
+try:
+    
+    palabra = str(input('Introduzca una palabra: '))
+    longitud = len(palabra)
 
-longitud = len(palabra)
-print(f'La lomgitud de tu palabra es: ',longitud)
+    print(f'La lomgitud de tu palabra es: ',longitud)
+
+except:
+    print ('Introduzca cadenas de texto no numeros o cualquier otro formato diferente')
