188b4c7f752b8c6393b9be6515b2333c67eb6c65
Javier Muñoz Parra <jmunpar1307@g.educaand.es>
2025-11-09 12:16:07 +0100

    añadido de ValueError

--- apartado2.1/cadenas/Longitud de una palabra.py
+++ apartado2.1/cadenas/Longitud de una palabra.py
@@ -1,9 +1,9 @@
 try:
-    
+
     palabra = str(input('Introduzca una palabra: '))
     longitud = len(palabra)
 
     print(f'La lomgitud de tu palabra es: ',longitud)
 
-except:
+except ValueError:
     print ('Introduzca cadenas de texto no numeros o cualquier otro formato diferente')
