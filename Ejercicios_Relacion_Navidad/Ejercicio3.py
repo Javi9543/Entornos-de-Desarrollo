6adf35595f020b8c1e9b81a52ea77234f0bae365
Javier Muñoz <jmunpar1307@g.educaand.es>
2026-01-04 13:26:45 +0100

    Comprobar la longitud de una cadena introducida por el usuario

---
+++ Ejercicios_Relacion_Navidad/Ejercicio3.py
@@ -0,0 +1,7 @@
+cadena = str(input("Introduzca una cadena para saber su longitud: "))
+contador = 0
+for i in cadena:
+    contador += 1
+
+print(f"La longitud de la cadena {cadena}, es de {contador} letras.")
+
