7e6b0a9ab28f97d9e5bd28ea9ac20a6b8371fba8
Javier Muñoz Parra <jmunpar1307@g.educaand.es>
2026-03-15 12:02:48 +0100

    creo el metodo constructor dejando el atributo estado con el valor Disponible por defecto, ya que cuando se cree un objeto libro Nuevo, el estado del libro este disponible

--- CodigoEjercicioBiblioteca/Libro.java
+++ CodigoEjercicioBiblioteca/Libro.java
@@ -3,4 +3,11 @@ public class Libro {
     private String nombre;
     private int numLibro;
     private String estado;
+
+    //metodo constructor
+    public Libro(String nombLibro, int numLibro){
+        this.nombre = nombLibro;
+        this.numLibro = numLibro;
+        estado = "Disponible";
+    }
 }
