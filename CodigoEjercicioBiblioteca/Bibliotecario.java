c0b022bdaf0e12dbeb7614ad221b5bebb606db97
Javier Muñoz Parra <jmunpar1307@g.educaand.es>
2026-03-15 12:22:13 +0100

    A esta clase, le añado el constructor que guarda el id del bibliotecario, los metotos crear Prestamo (coge el constructor de la clase prestamo para crear el prestamo del libro y un segundo metodo que registrar devolucion

--- CodigoEjercicioBiblioteca/Bibliotecario.java
+++ CodigoEjercicioBiblioteca/Bibliotecario.java
@@ -10,4 +10,33 @@ public class Bibliotecario {
         this.idBibliotecario = idBib;        
     }
 
+    //metodos de la clase
+    public void crearPrestamo(Cliente cliente, Libro libro){
+        new Presta
+        System.out.println( "Prestamo registrado por: " + this.nomBibliotecario);
+    }
+
+    public void registrarDevolucion(Libro libro){
+        libro.actualizarEstado("Disponible");
+        System.out.println("Devolucion Registrada correctamente");
+    }
+
+    //metodos get y set
+    public int getIdBibliotecario() {
+        return idBibliotecario;
+    }
+
+    public void setIdBibliotecario(int idBibliotecario) {
+        this.idBibliotecario = idBibliotecario;
+    }
+
+    public String getNomBibliotecario() {
+        return nomBibliotecario;
+    }
+
+    public void setNomBibliotecario(String nomBibliotecario) {
+        this.nomBibliotecario = nomBibliotecario;
+    }
+
+    
 }
