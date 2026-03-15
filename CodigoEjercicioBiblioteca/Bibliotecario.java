1e6a91ca2a0c04d637b5686f8f243b9823e7e2ce
Javier Muñoz Parra <jmunpar1307@g.educaand.es>
2026-03-15 12:15:39 +0100

    Creo la clase bibliotecario con sus atributos y metodo constructor

---
+++ CodigoEjercicioBiblioteca/Bibliotecario.java
@@ -0,0 +1,13 @@
+public class Bibliotecario {
+    //Atributos de la clase
+    private int idBibliotecario;
+    private String nomBibliotecario;
+
+    //metodo constructor
+    
+    public Bibliotecario(int idBib, String nombre) {
+        this.nomBibliotecario = nombre;
+        this.idBibliotecario = idBib;        
+    }
+
+}
